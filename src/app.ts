38d98e4bfe0442deeba8620ba68e536e809fea31
dylan rojo <devdylancrowder@outlook.com>
2024-07-23 20:28:25 -0300

    testing local

--- src/app.ts
+++ src/app.ts
@@ -1,45 +1,45 @@
 import express from "express";
-import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import jwt from "jsonwebtoken";
 import { v4 as uuidv4 } from "uuid";
-import cors from "cors";
-import cookieParser from "cookie-parser";
 
 const app = express();
-app.use(bodyParser.json());
-app.use(cookieParser()); // Añadir middleware para manejar cookies
+const SECRET_KEY = "your_secret_key";
 
+// Configurar CORS para permitir credenciales (cookies)
 app.use(
   cors({
     origin: "https://frontend-test-nine-xi.vercel.app",
-    methods: "GET,POST",
-    allowedHeaders: "Content-Type,Authorization",
     credentials: true,
   })
 );
 
-const SECRET_KEY = "tu_clave_secreta";
+app.use(cookieParser());
 
-app.use((req: any, res, next) => {
+app.use((req: any, res: any, next: any) => {
   const token = req.cookies.token;
   console.log("este es el token", token);
+
   if (!token) {
     const uuid = uuidv4();
-    const token: any = jwt.sign({ device: uuid }, SECRET_KEY, {
+    const newToken = jwt.sign({ device: uuid }, SECRET_KEY, {
       algorithm: "HS256",
       expiresIn: "30d",
     });
 
-    res.cookie("token", token, {
+    res.cookie("token", newToken, {
       httpOnly: true,
-      secure: true,
+      secure: true, // Asegúrate de estar usando HTTPS
       maxAge: 30 * 24 * 60 * 60 * 1000,
-      sameSite: "none",
+      sameSite: "Lax", // Considera cambiar a 'Strict' o 'None' según tus necesidades
     });
 
+    // Continúa con el siguiente middleware después de setear la cookie
     req.device = uuid;
     return next();
   }
+
   jwt.verify(token, SECRET_KEY, (err: any, decoded: any) => {
     if (err) {
       return res.status(403).send("Invalid token");
@@ -49,14 +49,12 @@ app.use((req: any, res, next) => {
   });
 });
 
-app.get("/token", (req: any, res) => {
-  res.json({
-    message: "This is sa protected route!! callate ",
-    device: req.device,
-  });
+// Rutas y lógica adicional
+app.get("/protected-route", (req: any, res: any) => {
+  // Ejemplo de ruta protegida
+  res.send("Acceso concedido");
 });
 
-const PORT = 8080;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(3000, () => {
+  console.log("Servidor escuchando en el puerto 3000");
 });
