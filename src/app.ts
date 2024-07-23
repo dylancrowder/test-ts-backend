e934662c02df6c80c248a3eddd0995ad6a1ae024
dylan rojo <devdylancrowder@outlook.com>
2024-07-23 18:13:01 -0300

    test nuevo

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,58 @@
 import express from "express";
+import bodyParser from "body-parser";
+import jwt from "jsonwebtoken";
+import { v4 as uuidv4 } from "uuid";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+app.use(bodyParser.json());
+app.use(cookieParser()); // Añadir middleware para manejar cookies
 
-app.get("/", (req, res) => {
-  res.send("esta conectado");
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET,POST",
+    allowedHeaders: "Content-Type,Authorization",
+    credentials: true,
+  })
+);
+
+const SECRET_KEY = "tu_clave_secreta";
+
+app.use((req: any, res, next) => {
+  const token = req.cookies.token;
+  console.log("este es el token", token);
+  if (!token) {
+    const uuid = uuidv4();
+    const token: any = jwt.sign({ device: uuid }, SECRET_KEY, {
+      algorithm: "HS256",
+      expiresIn: "30d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      sameSite: "none",
+    });
+
+    return res.json({ message: token });
+  }
+  jwt.verify(token, SECRET_KEY, (err: any, decoded: any) => {
+    if (err) {
+      return res.status(403).send("Invalid token");
+    }
+    req.device = decoded.device;
+    next();
+  });
 });
-const PORT: number = 8080;
+
+app.get("/token", (req: any, res) => {
+  res.json({ message: "This is a protected route", device: req.device });
+});
+
+const PORT = 8080;
 app.listen(PORT, () => {
-  console.log(`Server connected on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
