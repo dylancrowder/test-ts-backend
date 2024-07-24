d55ef2058694e3936735b58e18f625f0d58d73db
dylan rojo <devdylancrowder@outlook.com>
2024-07-24 01:26:22 -0300

    skere

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,22 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import { v4 as uuidv4 } from "uuid";
-
+import cors from "cors";
 const app = express();
 const SECRET_KEY = "your_secret_key";
 
 app.use(express.json());
-
+app.use(
+  cors({
+    origin: "https://vercel.com/dylancrowders-projects/frontend-test", // Permitir solo solicitudes desde este dominio
+    //   methods: ['GET', 'POST'], // Métodos HTTP permitidos
+    //   allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
+    //   exposedHeaders: ['Content-Length', 'X-Foo'], // Encabezados adicionales a exponer
+    credentials: true, // Habilitar credenciales de cookies/CORS
+    //   preflightContinue: false, // Continuar con preflight OPTIONS incluso si la ruta está manejada
+    //   optionsSuccessStatus: 204 // Establecer el código de estado para las respuestas OPTIONS exitosas
+  })
+);
 app.get("/", (req, res) => {
   // Genera un nuevo token y envíalo en la respuesta
   const uuid = uuidv4();
